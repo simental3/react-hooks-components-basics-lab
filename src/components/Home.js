6c60767a58dcb0266ca17c38e5c4211c8d9c55ae
Anthony Simental <anthonysimental12@gmail.com>
2023-04-27 13:21:58 -0700

    complete

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+{/* write an <About> component here */}
+
+export default Home
